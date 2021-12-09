60e67ef473a074c01d92e3059d12f5e2abbe0a40
yusuke <ID+username@users.noreply.github.com>
2021-12-09 01:46:22 -0800

    added test code

---
+++ server/src/test/mock/cardsObject.ts
@@ -0,0 +1,24 @@
+const cardsObject: TCard[] = [
+  {
+    suit: "H",
+    rank: "A",
+  },
+  {
+    suit: "H",
+    rank: "2",
+  },
+  {
+    suit: "H",
+    rank: "3",
+  },
+  {
+    suit: "D",
+    rank: "J",
+  },
+  {
+    suit: "C",
+    rank: "Q",
+  },
+];
+
+export default cardsObject;
