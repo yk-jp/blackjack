083cd9ac0eec899f2a12d2291ad645989cb8c0a3
yusuke <ID+username@users.noreply.github.com>
2021-12-11 16:55:30 -0800

    add types for table object fetched from server side

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 // components
 import Round from "../../components/Round/Round";
 import House from "../../components/House/House";
 import Player from "../../components/Player/Player";
-
+// dependencies
+import axios from "axios";
+// types
+import type ITable from "../../interfaces/ITable";
 const Main = () => {
+  useEffect(() => {
+    axios.get("http://localhost:5000/").then(function (response) {
+      // handle success
+      const table:ITable = response.data;
+      console.log(table.deck);
+    });
+  }, []);
+
   return (
     <main>
       <Round />
